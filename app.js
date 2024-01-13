3bcb6279f08e755c867f615a63a0d86bda1f2f58
Burakkturk <burak1845turk@icloud.com>
2024-01-13 22:45:34 +0300

    error

---
+++ app.js
@@ -0,0 +1,11 @@
+
+    console.log("Burası da calisir")
+
+    alert("Bu bir alert'dir")
+    karar = confirm("emin misiniz E/H")
+console.log(karar )
+
+    console.log("alertten sonraki komut")
+
+    console.warn("Bu bir uyarıdır")
+    console.error("  Eyvah hata olustu")
